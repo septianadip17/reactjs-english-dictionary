afab308bc5ba2cf4462c1f49daa2218f54dad444
Septian Adi Pratama <septianadip7998@gmail.com>
2023-09-06 11:44:44 +0700

    bug

--- src/App.js
+++ src/App.js
@@ -1,31 +1,107 @@
-import React from 'react'
+import React, {useState, useMemo, useEffect} from 'react'
 import Result from './Result'
 
+const synth = window.speechSynthesis;
+
 export const App = () => {
+  const voices = useMemo(() => synth.getVoices(), [])
+  const [voiceSelected, setVoiceSelected] = useState("Google US English")
+  const [text, setText] = useState("")
+  const [isSpeaking, setIsSpeaking] = useState("")
+  const [meanings, setMeanings] = useState([])
+  const [phonetics, setPhonetics] = useState([])
+  const [word, setWord] = useState("")
+  const [error, setError] = useState("")
+
+  const dictionaryApi = (text) => {
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${text}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(result => {
+      console.log(result)
+      setMeanings(result[0].meanings)
+      setPhonetics([0].phonetics)
+      setWord(result[0].word)
+      setError("")
+    })
+    .catch(err => setError(err))
+  }
+
+  const reset = () => {
+    setIsSpeaking("")
+    setError("")
+    setMeanings([])
+    setPhonetics([])
+    setWord("")
+  }
+
+  useEffect(() => {
+    if(!text.trim()) return reset();
+    const debounce = setTimeout(() => {
+      dictionaryApi(text)
+    }, 1000)
+
+    return () => clearTimeout(debounce)
+  }, [text])
+
+  const startSpeech = (text) => {
+    const utterance = new SpeechSynthesisUtterance(text);
+    const voice = voices.find(voice => voice.name === voiceSelected);
+    utterance.voice = voice;
+    synth.speak(utterance);
+    
+  }
+
+  const handleSpeech = () => {
+    if(!text.trim()) return;
+    if(!synth.speaking){
+      startSpeech(text)
+      setIsSpeaking("speak")
+    }else{
+      synth.cancel()
+    }
+
+    setInterval(() => {
+      if(!synth.speaking){
+        setIsSpeaking("")
+      }
+    }, 100)
+  }
+
   return (
     <div className="container">
       <h1>English Dictionary</h1>
 
       <form action="">
         <div className="row">
-          <textarea name="" id="" cols="30" rows="4" placeholder='Enter text'/>
+          <textarea cols="30" rows="4" placeholder='Enter text' value={text} onChange={e => setText(e.target.value)}/>
 
           <div className="voices-icons">
             <div className="select-voices">
-              <select name="" id="">
-                <option value="">English</option>
-                <option value="">English</option>
-                <option value="">English</option>
+              <select value={voiceSelected}
+              onChange={e => setVoiceSelected(e.target.value)}>
+                {
+                  voices.map(voice => (
+                <option key={voice.name} value={voice.name}>{voice.name}</option>
+                  ))
+                }
               </select>
             </div>
 
-            <i class="fa-solid fa-volume-high"></i>
+            <i className={`fa-solid fa-volume-high ${isSpeaking}`} onClick={handleSpeech}/>
           </div>
 
         </div>
       </form>
 
-      <Result/>
+      {
+        (text.trim() !== "" && !error) &&
+        <Result
+          word={word}
+          phonetics={phonetics}
+          meanings={meanings}
+          setText={setText}/>
+      }
     </div>
   )
 }
--- src/Result.js
+++ src/Result.js
@@ -1,26 +1,49 @@
 import React from 'react'
 
-const Result = () => {
+const Result = ({word, phonetics, meanings, setText}) => {
   return (
     <ul>
       <li className="world">
-        <h2>Hello</h2>
-        <span>həˈlō</span>
+        <h2>{word}</h2>
+        {
+          phonetics.map((phonetics, index) => (
+            <span key={index}>{phonetics.text}</span>
+          ))
+        }
       </li>
 
-      <li className="contain">
-        <h3>noun</h3> 
-
-        <div className="details-meaning">
-          <h3>Meaning</h3>
-          <p>- "Hello!" or an equivalent greeting.</p>
-        </div>
-
-        <div className="details-synonyms">
-          <h3>Synonyms</h3>
-          <span>greeting, greeting, greeting.</span>
-        </div>
-      </li>
+      {
+        meanings.map((meaning, index) => (
+          <li className="contain">
+            <h3>noun</h3> 
+    
+            <div className="details-meaning">
+              <h3>Meaning</h3>
+              {
+                meaning.definitions.map((definition, index) => (
+                  <p key={index}>- {definition.definition}</p>
+                  
+                ))
+              }
+            </div>
+            
+            {
+              meaning.synonyms.length !== 0 &&
+              <div className="details-synonyms">
+                <h3>Synonyms</h3>
+                {
+                  meaning.synonyms.map((synonym, index) => (
+                    <span key={index}
+                    onClick={() => setText(synonym)}>{`${synonym}, `}</span>
+                  ))
+                }
+            </div>
+            }
+    
+            
+          </li>
+        ))  
+      }
     </ul>
   )
 }
